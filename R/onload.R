429db1f26542f9b2337f3b6ad7b510a290af9b6b
Gábor Csárdi <csardi.gabor@gmail.com>
2018-09-13 01:24:07 +0100

    No need to import cli::cli

--- R/onload.R
+++ R/onload.R
@@ -3,8 +3,6 @@
 
 global_metadata_cache <- NULL
 
-#' @importFrom cli cli
-
 .onLoad <- function(libname, pkgname) {
   if (requireNamespace("debugme", quietly = TRUE)) debugme::debugme()
   global_metadata_cache <<- cranlike_metadata_cache$new()

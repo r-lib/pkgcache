c879d2acd21393d881a832399d897421a0f4a946
Gábor Csárdi <csardi.gabor@gmail.com>
2018-09-25 12:15:04 +0100

    Do not change warning.length option

--- R/onload.R
+++ R/onload.R
@@ -5,7 +5,6 @@ global_metadata_cache <- NULL
 
 .onLoad <- function(libname, pkgname) {
   backports::import(pkgname, "strrep")
-  options(warning.length = 8170)
   if (requireNamespace("debugme", quietly = TRUE)) debugme::debugme()
   global_metadata_cache <<- cranlike_metadata_cache$new()
 }

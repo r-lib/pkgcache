ae57982a3407a780905b8b6044c740f8e91335ca
Gábor Csárdi <csardi.gabor@gmail.com>
2018-11-16 12:06:18 +0000

    No need to import backports

--- R/onload.R
+++ R/onload.R
@@ -4,7 +4,6 @@
 global_metadata_cache <- NULL
 
 .onLoad <- function(libname, pkgname) {
-  backports::import(pkgname, "strrep")
   if (requireNamespace("debugme", quietly = TRUE)) debugme::debugme()
   global_metadata_cache <<- cranlike_metadata_cache$new()
 }

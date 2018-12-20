7a7d74db232387e4627a84dc2198adcbef6590fe
Gábor Csárdi <csardi.gabor@gmail.com>
2018-12-20 12:05:10 +0000

    More test cases

--- tests/testthat/test-metadata-api.R
+++ tests/testthat/test-metadata-api.R
@@ -3,4 +3,34 @@ context("metadata-api")
 
 test_that("metadata api", {
 
+  skip_if_offline()
+  if (packageVersion("callr") < "3.1.0.9000") skip("New newer callr")
+
+  tmp <- test_temp_dir()
+  withr::local_envvar(c(R_PKG_CACHE_DIR = tmp))
+  expect_equal(dir(tmp), character())
+
+  fun <- function() {
+    pkgcache::meta_cache_update()
+    ret1 <- pkgcache::meta_cache_list()
+    ret2 <- dir(Sys.getenv("R_PKG_CACHE_DIR"))
+    ret3 <- pkgcache::meta_cache_list("igraph")
+    ret4 <- pkgcache::meta_cache_deps("igraph")
+    ret5 <- pkgcache::meta_cache_revdeps("pkgconfig", recursive = FALSE)
+    ret6 <- pkgcache::meta_cache_cleanup(force = TRUE)
+    ret7 <- pkgcache::get_cranlike_metadata_cache()
+    list(class(ret1), ret2, ret3, ret4, ret5, NULL, class(ret7))
+  }
+
+  ret <- callr::r(fun)
+  expect_true("tbl_df" %in% ret[[1]])
+  expect_true("_metadata" %in% ret[[2]])
+  expect_s3_class(ret[[3]], "tbl_df")
+  expect_true("igraph" %in% ret[[3]]$package)
+  expect_true("igraph" %in% ret[[4]]$package)
+  expect_true("pkgconfig" %in% ret[[5]]$package)
+  expect_true("cranlike_metadata_cache" %in% ret[[7]])
+  expect_true("R6" %in% ret[[7]])
+
+  expect_false("_metadata" %in% dir(tmp))
 })
---
+++ tests/testthat/test-metadata-errors.R
@@ -0,0 +1,24 @@
+
+context("metadata-errors")
+
+test_that("invalid PACKAGES file warns", {
+  pri <- test_temp_dir()
+  rep <- test_temp_dir()
+
+  cmc <- cranlike_metadata_cache$new(
+    pri, rep, platforms = "source", bioc = FALSE)
+
+  pri_files <- get_private(cmc)$get_cache_files("primary")
+  rep_files <- get_private(cmc)$get_cache_files("replica")
+
+  for (pgz in rep_files$pkgs$path) {
+    mkdirp(dirname(pgz))
+    writeBin(charToRaw("nope"), pgz)
+  }
+
+  expect_error(
+    expect_warning(
+      get_private(cmc)$update_replica_rds(),
+      "Cannot read metadata information"),
+    "No metadata available")
+})

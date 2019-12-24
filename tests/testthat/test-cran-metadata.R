7f2a2b5404514e80322306ab7bb91d20a7eb279a
Gábor Csárdi <csardi.gabor@gmail.com>
2019-12-24 10:46:06 +0000

    Make the r-pkg.org failure test more robust

--- tests/testthat/test-cran-metadata.R
+++ tests/testthat/test-cran-metadata.R
@@ -4,18 +4,24 @@ context("CRAN metadata")
 test_that("what if cran.r-pkg.org is down?", {
   skip_if_offline()
 
+  dir.create(pri <- fs::path_norm(tempfile()))
+  on.exit(unlink(pri, recursive = TRUE), add = TRUE)
+  dir.create(rep <- fs::path_norm(tempfile()))
+  on.exit(unlink(rep, recursive = TRUE), add = TRUE)
+
+  cmc <- cranlike_metadata_cache$new(pri, rep, "source", bioc = FALSE)
+
+  # First get the metadata, so updates are fast
+  expect_error(cmc$update(), NA)
+
+  # Then simulate a broken web site, giving 5 secs to the non-broken ones
   env <- c(
     "R_PKG_CRAN_METADATA_URL" = "http://192.0.2.0/",
-    "PKGCACHE_TIMEOUT" = "1",
-    "PKGCACHE_CONNECTTIMEOUT" = "1"
+    "PKGCACHE_TIMEOUT" = "5",
+    "PKGCACHE_CONNECTTIMEOUT" = "2"
   )
 
   withr::with_envvar(env, {
-    dir.create(pri <- fs::path_norm(tempfile()))
-    on.exit(unlink(pri, recursive = TRUE), add = TRUE)
-    dir.create(rep <- fs::path_norm(tempfile()))
-    on.exit(unlink(rep, recursive = TRUE), add = TRUE)
-    cmc <- cranlike_metadata_cache$new(pri, rep, "source", bioc = FALSE)
     tic <- Sys.time()
     expect_error(cmc$update(), NA)
     toc <- Sys.time()

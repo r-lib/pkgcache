2e4c0d827bbda07795b32e802b15cbf34ad93774
Gábor Csárdi <csardi.gabor@gmail.com>
2020-10-09 09:32:38 +0100

    Fix tests with new testthat

--- tests/test-async.R
+++ tests/test-async.R
@@ -4,19 +4,9 @@ if (file.exists("async")) {
   library(pkgcache)
   print(sessioninfo::package_info("pkgcache", dependencies = TRUE))
 
-  test <- function() {
-    package <- "pkgcache"
-    env_test <- asNamespace("testthat")$env_test
-    env_test$in_test <- TRUE
-    env_test$package <- package
-    on.exit({
-      env_test$in_test <- FALSE
-      env_test$package <- NULL
-    })
-    test_path <- "async"
-    asNamespace("testthat")$test_package_dir(
-      package = package, test_path = test_path,
-      filter = NULL, reporter = "summary")
-  }
-  test()
+  test_dir(
+    "async",
+    env = new.env(parent = asNamespace("pkgcache")),
+    reporter = "check"
+  )
 }

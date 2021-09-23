4ec82cb8d8aaea550a0c69e5c22d2615b313d30d
Gábor Csárdi <csardi.gabor@gmail.com>
2021-09-23 14:19:24 +0200

    Skip a flaky async test case
    We don't use this functionality anywhere currently.

--- tests/async/test-process.R
+++ tests/async/test-process.R
@@ -28,6 +28,7 @@ test_that("process", {
 
 test_that("process + http", {
   skip_on_cran()
+  skip("async processes are a bit flaky")
   px <- asNamespace("processx")$get_tool("px")
 
   afun <- async(function() {

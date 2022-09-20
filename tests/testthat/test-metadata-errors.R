33cf459da61535d9120f2e46e2d212b2156de103
Gábor Csárdi <csardi.gabor@gmail.com>
2022-09-20 12:50:28 +0200

    Fix a test case on Windows
    IDK why this worked on Unix, but not on Windows,
    but it is best to catch the warning explicitly,
    anyway.

--- tests/testthat/test-metadata-errors.R
+++ tests/testthat/test-metadata-errors.R
@@ -14,13 +14,20 @@ test_that("invalid PACKAGES file warns", {
     writeBin(charToRaw("nope"), pgz)
   }
 
-  suppressMessages(
-    expect_error(
-      expect_warning(
-        get_private(cmc)$update_replica_rds(),
-        "Cannot read metadata information"
-      ),
-      "No metadata available"
-    )
+  warn <- NULL
+  expect_error(
+    withCallingHandlers(
+      suppressMessages(get_private(cmc)$update_replica_rds()),
+      warning = function(w) {
+        warn <<- w
+        invokeRestart("muffleWarning")
+      }
+    ),
+    "No metadata available"
+  )
+
+  expect_match(
+    conditionMessage(warn),
+    "Cannot read metadata information"
   )
 })

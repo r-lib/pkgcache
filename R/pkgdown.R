b61c9aee78b47b3470e152c513da43952510e623
Gábor Csárdi <csardi.gabor@gmail.com>
2023-03-08 13:20:07 +0100

    Need to qualify utils::packageVersion()

--- R/pkgdown.R
+++ R/pkgdown.R
@@ -1,6 +1,6 @@
 
 pkgdown_url <- function() {
-  ver <- packageVersion(.packageName)
+  ver <- utils::packageVersion(.packageName)
   paste0(
     "https://r-lib.github.io/pkgcache",
     if (!is.na(ver[,4])) "/dev"

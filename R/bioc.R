e00e752f96782ff34e3cab712aaae79c938b4f8d
Gábor Csárdi <csardi.gabor@gmail.com>
2023-03-28 15:27:17 +0200

    Update builtin R -> Bioc map

--- R/bioc.R
+++ R/bioc.R
@@ -112,7 +112,8 @@ bioconductor <- local({
     "4.0"  = package_version("3.12"),
     "4.1"  = package_version("3.14"),
     "4.2"  = package_version("3.16"),
-    "4.3"  = package_version("3.17")
+    "4.3"  = package_version("3.17"),
+    "4.4"  = package_version("3.17")
   )
 
   # -------------------------------------------------------------------

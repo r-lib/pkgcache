a6eb457363a0b679a2794781aab344022a4fa1bb
Gábor Csárdi <csardi.gabor@gmail.com>
2023-04-18 12:39:05 +0200

    Fix URL redirection
    [ci skip]

--- R/pkgdown.R
+++ R/pkgdown.R
@@ -2,7 +2,7 @@
 pkgdown_url <- function() {
   ver <- utils::packageVersion(.packageName)
   paste0(
-    "https://r-lib.github.io/pkgcache",
-    if (!is.na(ver[,4])) "/dev"
+    "https://r-lib.github.io/pkgcache/",
+    if (!is.na(ver[,4])) "dev/"
   )
 }

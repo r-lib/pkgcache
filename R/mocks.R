d969bca25d3d304d3cc6452ca0fdd5cffdd17aa6
Jon Harmon <jonthegeek@gmail.com>
2024-08-15 15:13:06 -0700

    Make R CMD check happier.

--- R/mocks.R
+++ R/mocks.R
@@ -1,7 +1,11 @@
 # Define these objects so they can be mocked in tests.
 interactive <- interactive
-readline <- readline
+readline <- function(prompt = "") {
+  readline
+}
 length <- length
-Sys.info <- Sys.info
+Sys.info <- function() {
+  Sys.info
+}
 R_user_dir <- R_user_dir
 getRversion <- getRversion

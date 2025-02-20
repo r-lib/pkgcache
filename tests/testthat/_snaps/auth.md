71093376d265862c310221569358ff4ab32314cb
Gábor Csárdi <csardi.gabor@gmail.com>
2025-02-20 18:45:34 +0100

    Simplify auth tests

---
+++ tests/testthat/_snaps/auth.md
@@ -0,0 +1,154 @@
+# looking up auth headers for repositories works as expected
+
+    Code
+      repo_auth_headers(
+        "https://username@ppm.internal/cran/__linux__/jammy/latest/src/contrib/PACKAGES.gz",
+        allow_prompt = FALSE, use_cache = FALSE, set_cache = FALSE)
+    Output
+      $headers
+                     Authorization 
+      "Basic dXNlcm5hbWU6dG9rZW4=" 
+      
+      $auth_domain
+      [1] "https://ppm.internal/cran/latest"
+      
+
+---
+
+    Code
+      repo_auth_headers(
+        "https://username@ppm.internal/cran/latest/bin/linux/4.4-jammy/contrib/4.4/PACKAGES.gz",
+        allow_prompt = FALSE, use_cache = FALSE, set_cache = FALSE)
+    Output
+      $headers
+                     Authorization 
+      "Basic dXNlcm5hbWU6dG9rZW4=" 
+      
+      $auth_domain
+      [1] "https://ppm.internal/cran/latest"
+      
+
+# caching
+
+    Code
+      repo_auth_headers(
+        "https://username@ppm.internal/cran/__linux__/jammy/latest/src/contrib/PACKAGES.gz",
+        allow_prompt = FALSE, use_cache = TRUE, set_cache = TRUE)
+    Output
+      $headers
+                     Authorization 
+      "Basic dXNlcm5hbWU6dG9rZW4=" 
+      
+      $auth_domain
+      [1] "https://ppm.internal/cran/latest"
+      
+    Code
+      pkgenv$credentials[["https://ppm.internal/cran/latest"]]
+    Output
+      $headers
+                     Authorization 
+      "Basic dXNlcm5hbWU6dG9rZW4=" 
+      
+      $auth_domain
+      [1] "https://ppm.internal/cran/latest"
+      
+
+---
+
+    Code
+      repo_auth_headers(
+        "https://username@ppm.internal/cran/latest/bin/linux/4.4-jammy/contrib/4.4/PACKAGES.gz",
+        allow_prompt = FALSE, use_cache = TRUE, set_cache = TRUE)
+    Output
+      $headers
+                     Authorization 
+      "Basic dXNlcm5hbWU6dG9rZW4=" 
+      
+      $auth_domain
+      [1] "https://ppm.internal"
+      
+    Code
+      pkgenv$credentials[["https://ppm.internal"]]
+    Output
+      $headers
+                     Authorization 
+      "Basic dXNlcm5hbWU6dG9rZW4=" 
+      
+      $auth_domain
+      [1] "https://ppm.internal"
+      
+
+# basic auth credentials can be extracted from various URL formats
+
+    Code
+      extract_basic_auth_credentials("https://user.name:pass-word123@example.com")
+    Output
+      $hosturl
+      [1] "https://example.com"
+      
+      $hostuserurl
+      [1] "https://user.name@example.com"
+      
+      $repourl
+      [1] "https://example.com"
+      
+      $repouserurl
+      [1] "https://user.name@example.com"
+      
+      $username
+      [1] "user.name"
+      
+      $password
+      [1] "pass-word123"
+      
+    Code
+      extract_basic_auth_credentials("http://user@example.com")
+    Output
+      $hosturl
+      [1] "http://example.com"
+      
+      $hostuserurl
+      [1] "http://user@example.com"
+      
+      $repourl
+      [1] "http://example.com"
+      
+      $repouserurl
+      [1] "http://user@example.com"
+      
+      $username
+      [1] "user"
+      
+      $password
+      [1] ""
+      
+    Code
+      extract_basic_auth_credentials("https://example.com")
+    Output
+      $hosturl
+      [1] "https://example.com"
+      
+      $hostuserurl
+      [1] "https://example.com"
+      
+      $repourl
+      [1] "https://example.com"
+      
+      $repouserurl
+      [1] "https://example.com"
+      
+      $username
+      [1] ""
+      
+      $password
+      [1] ""
+      
+
+---
+
+    Code
+      extract_basic_auth_credentials("notaurl")
+    Condition
+      Error:
+      ! Unrecognized URL format: `notaurl`.
+
--- tests/testthat/test-auth.R
+++ tests/testthat/test-auth.R
@@ -27,37 +27,25 @@ test_that("looking up auth headers for repositories works as expected", {
   )
 
   # Verify that the environment variable keyring backend is picked up correctly.
-  withr::with_envvar(
-    c("https://ppm.internal/cran/latest:username" = "token"),
-    expect_equal(
-      repo_auth_headers(
-        "https://username@ppm.internal/cran/__linux__/jammy/latest/src/contrib/PACKAGES.gz",
-        allow_prompt = FALSE,
-        use_cache = FALSE,
-        set_cache = FALSE
-      ),
-      list(
-        headers = c("Authorization" = "Basic dXNlcm5hbWU6dG9rZW4="),
-        auth_domain = "https://ppm.internal/cran/latest"
-      )
+  withr::local_envvar(c("https://ppm.internal/cran/latest:username" = "token"))
+  expect_snapshot(
+    repo_auth_headers(
+      "https://username@ppm.internal/cran/__linux__/jammy/latest/src/contrib/PACKAGES.gz",
+      allow_prompt = FALSE,
+      use_cache = FALSE,
+      set_cache = FALSE
     )
   )
 
   # Verify that we fall back to checking for a hostname credential when none
   # is available for a specific repo.
-  withr::with_envvar(
-    c("https://ppm.internal:username" = "token"),
-    expect_equal(
-      repo_auth_headers(
-        "https://username@ppm.internal/cran/latest/bin/linux/4.4-jammy/contrib/4.4/PACKAGES.gz",
-        allow_prompt = FALSE,
-        use_cache = FALSE,
-        set_cache = FALSE
-      ),
-      list(
-        headers = c("Authorization" = "Basic dXNlcm5hbWU6dG9rZW4="),
-        auth_domain = "https://ppm.internal"
-      )
+  withr::local_envvar(c("https://ppm.internal:username" = "token"))
+  expect_snapshot(
+    repo_auth_headers(
+      "https://username@ppm.internal/cran/latest/bin/linux/4.4-jammy/contrib/4.4/PACKAGES.gz",
+      allow_prompt = FALSE,
+      use_cache = FALSE,
+      set_cache = FALSE
     )
   )
 })
@@ -66,90 +54,40 @@ test_that("caching", {
   on.exit(clear_auth_cache(), add = TRUE)
 
   clear_auth_cache()
-  withr::with_envvar(
-    c("https://ppm.internal/cran/latest:username" = "token"),
-    expect_equal(
-      repo_auth_headers(
-        "https://username@ppm.internal/cran/__linux__/jammy/latest/src/contrib/PACKAGES.gz",
-        allow_prompt = FALSE,
-        use_cache = TRUE,
-        set_cache = TRUE
-      ),
-      list(
-        headers = c("Authorization" = "Basic dXNlcm5hbWU6dG9rZW4="),
-        auth_domain = "https://ppm.internal/cran/latest"
-      )
+  withr::local_envvar(c("https://ppm.internal/cran/latest:username" = "token"))
+  expect_snapshot({
+    repo_auth_headers(
+      "https://username@ppm.internal/cran/__linux__/jammy/latest/src/contrib/PACKAGES.gz",
+      allow_prompt = FALSE,
+      use_cache = TRUE,
+      set_cache = TRUE
     )
-  )
-  expect_equal(
-    pkgenv$credentials[["https://ppm.internal/cran/latest"]],
-    list(
-      headers = c("Authorization" = "Basic dXNlcm5hbWU6dG9rZW4="),
-      auth_domain = "https://ppm.internal/cran/latest"
-    )
-  )
+    pkgenv$credentials[["https://ppm.internal/cran/latest"]]
+  })
 
   clear_auth_cache()
-  withr::with_envvar(
-    c("https://ppm.internal:username" = "token"),
-    expect_equal(
-      repo_auth_headers(
-        "https://username@ppm.internal/cran/latest/bin/linux/4.4-jammy/contrib/4.4/PACKAGES.gz",
-        allow_prompt = FALSE,
-        use_cache = TRUE,
-        set_cache = TRUE
-      ),
-      list(
-        headers = c("Authorization" = "Basic dXNlcm5hbWU6dG9rZW4="),
-        auth_domain = "https://ppm.internal"
-      )
+  withr::local_envvar(c(
+    "https://ppm.internal:username" = "token",
+    "https://ppm.internal/cran/latest:username" = NA_character_
+  ))
+  expect_snapshot({
+    repo_auth_headers(
+      "https://username@ppm.internal/cran/latest/bin/linux/4.4-jammy/contrib/4.4/PACKAGES.gz",
+      allow_prompt = FALSE,
+      use_cache = TRUE,
+      set_cache = TRUE
     )
-  )
-  expect_equal(
-    pkgenv$credentials[["https://ppm.internal"]],
-    list(
-      headers = c("Authorization" = "Basic dXNlcm5hbWU6dG9rZW4="),
-      auth_domain = "https://ppm.internal"
-    )
-  )
+    pkgenv$credentials[["https://ppm.internal"]]
+  })
 })
 
 test_that("basic auth credentials can be extracted from various URL formats", {
-  expect_equal(
-    extract_basic_auth_credentials("https://user.name:pass-word123@example.com"),
-    list(
-      hosturl = "https://example.com",
-      hostuserurl = "https://user.name@example.com",
-      repourl = "https://example.com",
-      repouserurl = "https://user.name@example.com",
-      username = "user.name",
-      password = "pass-word123"
-    )
-  )
-  expect_equal(
-    extract_basic_auth_credentials("http://user@example.com"),
-    list(
-      hosturl = "http://example.com",
-      hostuserurl = "http://user@example.com",
-      repourl = "http://example.com",
-      repouserurl = "http://user@example.com",
-      username = "user",
-      password = ""
-    )
-  )
-  expect_equal(
-    extract_basic_auth_credentials("https://example.com"),
-    list(
-      hosturl = "https://example.com",
-      hostuserurl = "https://example.com",
-      repourl = "https://example.com",
-      repouserurl = "https://example.com",
-      username = "",
-      password = ""
-    )
-  )
-  expect_error(
-    extract_basic_auth_credentials("notaurl"),
-    "Unrecognized URL format"
-  )
+  expect_snapshot({
+    extract_basic_auth_credentials("https://user.name:pass-word123@example.com")
+    extract_basic_auth_credentials("http://user@example.com")
+    extract_basic_auth_credentials("https://example.com")
+  })
+  expect_snapshot(error = TRUE, {
+    extract_basic_auth_credentials("notaurl")
+  })
 })

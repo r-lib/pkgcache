6b9e7f437f8b65f1b48e026fc3c403bb8fa31697
Gábor Csárdi <csardi.gabor@gmail.com>
2025-02-20 14:23:48 +0100

    Add auth to the archive cache and ppm functions

--- R/metadata-cache.R
+++ R/metadata-cache.R
@@ -754,14 +754,14 @@ cmc__update_replica_pkgs <- function(self, private) {
 
   key <- random_key()
   async_constant()$
-    finally(function() clear_auth_cache(key))$
     then(function() start_auth_cache(key))$
     then(function() download_files(dls))$
     then(function(result) {
       missing_pkgs_note(pkgs, result)
       load_bioc_sysreqs()
       result
-    })
+    })$
+    finally(function() clear_auth_cache(key))
 }
 
 # E.g. "R 4.1 macos packages are missing from CRAN and Bioconductor"

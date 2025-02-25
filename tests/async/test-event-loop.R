5efd7dca16fb915d5fec0e51b305a5eb4203bb49
Gábor Csárdi <csardi.gabor@gmail.com>
2025-02-25 13:02:48 +0100

    Update embedded async

--- tests/async/test-event-loop.R
+++ tests/async/test-event-loop.R
@@ -52,10 +52,7 @@ test_that("repeated delay", {
   expect_null(error)
   expect_equal(result, 1:10)
   expect_true(end - start >= as.difftime(1, units = "secs"))
-  expect_true(end - start <= as.difftime(3, units = "secs"))
-  if (end - start > as.difftime(3, units = "secs")) {
-    message("Took ", end - start, " secs.")
-  }
+  expect_true(end - start <= as.difftime(2, units = "secs"))
 })
 
 test_that("nested event loops", {

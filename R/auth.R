c096c707e2c723fc3e92019d336d979dc4069244
Gábor Csárdi <csardi.gabor@gmail.com>
2025-02-27 09:09:48 +0100

    Implement `check_credentials` in `repo_auth()`

--- R/auth.R
+++ R/auth.R
@@ -67,16 +67,28 @@ repo_auth <- function(r_version = getRversion(), bioc = TRUE,
   res$auth_error <- rep(NA_character_, nrow(res))
   for (w in seq_len(nrow(res))) {
     url <- res$url[w]
-    cred <- repo_auth_headers(url, warn = FALSE)
-    if (is.null(cred)) next
-    res$username[w] <- cred$username
-    res$has_password[w] <- cred$found
-    res$auth_domains[w] <- list(cred$auth_domains)
-    if (cred$found) {
-      res$auth_source[w] <- cred$source
-      res$auth_domain[w] <- cred$auth_domain
+    if (check_credentials) {
+      cred <- repo_auth_headers(url, warn = FALSE)
+      if (is.null(cred)) next
+      res$username[w] <- cred$username
+      res$has_password[w] <- cred$found
+      res$auth_domains[w] <- list(cred$auth_domains)
+      if (cred$found) {
+        res$auth_source[w] <- cred$source
+        res$auth_domain[w] <- cred$auth_domain
+      } else {
+        res$auth_error[w] <- cred$error
+      }
     } else {
-      res$auth_error[w] <- cred$error
+      parsed_url <- parse_url_basic_auth(url)
+      if (length(parsed_url$username) == 0 ||
+          nchar(parsed_url$username) == 0) {
+        next
+      }
+      res$username[w] <- parsed_url$username
+      res$auth_domains[w] <- list(unique(unlist(
+        parsed_url[c("repouserurl", "repourl", "hostuserurl", "hosturl")]
+      )))
     }
   }
 
